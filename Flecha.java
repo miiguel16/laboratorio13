3ba0569eea7d21330d7b9e8e2d9790bf7974c592
JGoncu97 <jggc1197@gmail.com>
2024-10-11 15:57:38 -0500

    finish

---
+++ Flecha.java
@@ -0,0 +1,41 @@
+
+public class Flecha {
+
+	int longitud;
+	String color;
+	
+	public Flecha() {
+		longitud=18;
+		color="negro";
+		
+		
+	}
+	
+	public Flecha(int longitud, String color) {
+		this.longitud=longitud;
+		this.color=color;
+		
+	}
+	
+	public void imprimirEspacion() {
+		System.out.println();
+	}
+	
+	public void construirFlecha() {
+		for (int i = 0; i < longitud; i++) {
+			imprime("-");
+			
+		}
+		imprime(">");
+	}
+	
+	private void imprime(String simbolo) {
+		if (color.equals("negro")) {
+			System.out.print(simbolo);
+		}else {
+			System.err.print(simbolo);
+		}
+		
+	}
+
+}
